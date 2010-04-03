a7d56dcdd1556eb8ea5d22d9afd2aea1d4c2ecbb
Joao Costa <jcosta@localhost.localdomain>
2010-04-03 05:13:52 +0100

    Refactor Datasource.pm to include refactor function

--- perl_modules/Finance/HostedTrader/Datasource.pm
+++ perl_modules/Finance/HostedTrader/Datasource.pm
@@ -131,6 +131,45 @@ ON DUPLICATE KEY UPDATE open=values(open), low=values(low), high=values(high), c
     $self->{'dbh'}->do($sql);
 }
 
+sub createSynthetic {
+my ($self, $synthetic, $timeframe) = @_;
+
+my $symbols = $self->getNaturalSymbols;
+my $sym1 = substr($synthetic,0,3);
+my $sym2 = substr($synthetic,3,3);
+my $search1=$sym1.'USD';
+my $search2='USD'.$sym1;
+my @u1 = grep(/$search1|$search2/, @$symbols);
+
+$search1=$sym2.'USD';
+$search2='USD'.$sym2;
+my @u2 = grep(/$search1|$search2/, @$symbols);
+my $op;
+my ($low,$high);
+if ($u1[0] =~ /USD$/ && $u2[0] =~ /^USD/) {
+  $op = '*';
+  $low='low';$high='high';
+} elsif ($u1[0] =~ /USD$/ && $u2[0] =~ /USD$/) {
+  $op = '/';
+  $low='high';$high='low';
+} elsif ($u1[0] =~ /^USD/ && $u2[0] =~ /^USD/) {
+  $op = '/';
+  $low='high';$high='low';
+  my $tmp = $u1[0];
+  $u1[0] = $u2[0];
+  $u2[0] = $tmp;
+} else {
+  warn "Don't know how to handle $synthetic [] synthetic pair\n";
+  next;
+}
+
+my $filter = ' AND T1.datetime > DATE_SUB(NOW(), INTERVAL 2 WEEK)';
+
+my $sql = "insert ignore into $synthetic\_$timeframe (select T1.datetime, round(T1.open".$op."T2.open,4) as open, round(T1.low".$op."T2.$low,4) as low, round(T1.high".$op."T2.$high,4) as high, round(T1.close".$op."T2.close,4) as close from $u1[0]\_$timeframe as T1, $u2[0]\_$timeframe as T2 WHERE T1.datetime = T2.datetime $filter)";
+
+$self->{dbh}->do($sql);
+}
+
 sub getNaturalSymbols {
     my $self = shift;
     my $symbols= $self->{cfg}->{symbols}->{natural} || die('No symbols defined in config file');
