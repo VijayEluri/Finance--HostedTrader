791d4b276e62dfdfe53cfa5e77e1589fd00859b1
Joao Costa <jcosta@localhost.localdomain>
2010-04-04 03:06:10 +0100

    POD for eval.pl script

--- eval.pl
+++ eval.pl
@@ -1,4 +1,70 @@
 #!/usr/bin/perl
+=head1 NAME
+
+Outputs the value of an indicator against all known symbols
+
+
+=head1 SYNOPSIS
+
+    eval.pl [--timeframe=tf] [--verbose] [--symbols=s] [--debug] [--max-loaded-items=i] [--max-display-items=i] expr
+
+
+=head1 DESCRIPTION
+
+Sample expressions:
+
+rsi(close,14)
+
+ema(close,21)
+
+
+=head2 OPTIONS
+
+=over 12
+
+=item C<--timeframe=tf>
+
+Required argument. Specifies a single timeframe for which
+synthetic data will be created.
+
+tf can be a valid integer timeframe as defined in L<Finance::HostedTrader::Datasource>
+
+=item C<--symbols=s>
+
+Comma separated list of symbols for which to run the indicator against.
+If not supplied, defaults to the list entry in the config file item "symbols.synthetic" and "symbols.natural".
+
+=item C<--help>
+
+Display usage information.
+
+=item C<--debug>
+
+Debug output.
+
+=item C<--max-loaded-items=i>
+
+Number of database records to load. Defaults to 1000 which should be enough to calculate any indicator.
+
+=item C<--max-display-items=i>
+
+Number of indicator periods to display. Defaults to one (eg: only display the indicator value today)
+
+=back
+
+=head1 LICENSE
+
+This is released under the MIT license. See L<http://www.opensource.org/licenses/mit-license.php>.
+
+=head1 AUTHOR
+
+Joao Costa - L<http://zonalivre.org/>
+
+=head1 SEE ALSO
+
+L<Finance::HostedTrader::Datasource>
+
+=cut
 
 use strict;
 use warnings;
@@ -8,9 +74,10 @@ use Finance::HostedTrader::ExpressionParser;
 
 use Data::Dumper;
 use Getopt::Long;
+use Pod::Usage;
 
-my ( $timeframe, $max_loaded_items, $max_display_items, $symbols_txt, $debug ) =
-  ( 'day', 1000, 1, '', 0 );
+my ( $timeframe, $max_loaded_items, $max_display_items, $symbols_txt, $debug, $help ) =
+  ( 'day', 1000, 1, '', 0, 0 );
 
 GetOptions(
     "timeframe=s"         => \$timeframe,
@@ -18,7 +85,8 @@ GetOptions(
     "symbols=s"           => \$symbols_txt,
     "max-loaded-items=i"  => \$max_loaded_items,
     "max-display-items=i" => \$max_display_items,
-) || exit(1);
+) || pod2usage(2);
+pod2usage(1) if ($help);
 
 my $cfg               = Finance::HostedTrader::Config->new();
 my $signal_processor = Finance::HostedTrader::ExpressionParser->new();
