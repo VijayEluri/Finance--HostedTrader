a00358e88252c4dfe6d766e31ad86d0bd6ca959c
Joao Costa <joaocosta@zonalivre.org>
2010-04-11 02:44:50 +0000

    Use underscores instead of dots otherwise mysqlimport cannot directly load data

--- data/forexite/process.forexite.sh
+++ data/forexite/process.forexite.sh
@@ -7,5 +7,5 @@ SYMBOLS=`echo $SYMBOLS | perl -ne 'print'`
 
 
 for symbol in $SYMBOLS; do
-grep $symbol $1 | cut -d , -f 2,3,4,5,6,7 | perl -ne 'chomp();($d,$t,$o,$h,$l,$c)=split(",");$d=substr($d,0,4)."-".substr($d,4,2)."-".substr($d,6,2);$t=substr($t,0,2).":".substr($t,2,2).":".substr($t,4,2);print "$d $t,$o,$l,$h,$c\n"' | sort >> $symbol.60
+grep $symbol $1 | cut -d , -f 2,3,4,5,6,7 | perl -ne 'chomp();($d,$t,$o,$h,$l,$c)=split(",");$d=substr($d,0,4)."-".substr($d,4,2)."-".substr($d,6,2);$t=substr($t,0,2).":".substr($t,2,2).":".substr($t,4,2);print "$d $t,$o,$l,$h,$c\n"' | sort >> $symbol_60
 done
