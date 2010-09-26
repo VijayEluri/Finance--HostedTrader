c6c9e83918e896e8029c30faba8fd3aa37465843
Joao Costa <joaocosta@zonalivre.org>
2010-09-26 19:25:47 +0000

    Better data checking In addition to checking processes are running, also check datafeed data is up to date and FXCM API server is responsive

--- data/fxcm/servers/checkProcesses.sh
+++ data/fxcm/servers/checkProcesses.sh
@@ -12,3 +12,18 @@ for p in Trader.pl RatePrinter.exe Server.exe; do
         echo Not running $p
     fi
 done
+
+
+#check if data is up to date
+LAST_TICK=`mysql -N -ufxcm -e "SELECT MAX(datetime) FROM XAUUSD_300" fxcm`
+perl -MDate::Manip -e 'my $timediff=Delta_Format(DateCalc($ARGV[0], "now"),0,"%sh"); print "Datafeed out of date\n" if ($timediff > 1200)' "$LAST_TICK" 
+
+#check if the FXCM API server is responding to requests
+RES=`echo ask XAUUSD | nc -w 3 localhost 1500`
+if [[ "$RES" = "" ]]; then
+    echo Server timeout
+else
+    if [[ "${RES:0:4}" != "200 " ]]; then
+        echo Server returned bad response "$RES"
+    fi
+fi
