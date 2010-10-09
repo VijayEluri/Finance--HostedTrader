bc8e7e86bddbfcd85e91aad33872d0f0e2957a0b
Joao Costa <joaocosta@zonalivre.org>
2010-10-09 03:20:50 +0000

    Work in progress in making these function generic

--- data/fxcm/servers/Trader/pickSyms.pl
+++ data/fxcm/servers/Trader/pickSyms.pl
@@ -12,17 +12,99 @@ use Finance::HostedTrader::ExpressionParser;
 
 
 my $newSymbols = getSymbolsTrendFollow();
+$newSymbols = filterTopX($newSymbols, 5);
+
 my $system = Systems->new( name => 'trendfollow' );
 $system->updateSymbols($newSymbols);
 
 
-#$newSymbols = getSymbolsTrendFollow();
 #$system = Systems->new( name => 'countertrend');
 #$system->updateSymbols($newSymbols);
 
 
 #Return list of symbols to add to the system
 sub getSymbolsTrendFollow {
+    my $symbols = getAllSymbols();
+    my $processor   = Finance::HostedTrader::ExpressionParser->new();
+
+    my $rv = { long => [], short => [] };
+
+    foreach my $symbol (@$symbols) {
+        if ($processor->checkSignal( {
+            'expr' => 'trend(close,21) > 1',
+            'symbol' => $symbol,
+            'timeframe' => 'week',
+            'maxLoadedItems' => 50,
+            'period' => '8days',
+            'debug' => 0,
+        })) {
+            push @{ $rv->{long} }, $symbol;
+        } elsif ($processor->checkSignal( {
+            'expr' => 'trend(close,21) < -1',
+            'symbol' => $symbol,
+            'timeframe' => 'week',
+            'maxLoadedItems' => 50,
+            'period' => '8days',
+            'debug' => 0,
+        })) {
+            push @{ $rv->{short} }, $symbol;
+        }
+    }
+
+    return $rv;
+}
+
+sub filterTopX {
+    my $existing = shift;
+    my $number_to_keep = shift;
+    my @results;
+    my $processor   = Finance::HostedTrader::ExpressionParser->new();
+
+    my $calculateIndicator = sub {
+        my $direction = shift;
+        foreach my $symbol (@{ $existing->{$direction} }) {
+            my $data = $processor->getIndicatorData( {
+                'fields'          => "datetime,abs(trend(close,21))",
+                'symbol'        => $symbol,
+                'tf'            => 'week',
+                'maxLoadedItems'=> 41,
+                'numItems'      => 1,
+                'debug'         => 0,
+            } );
+            $data = $data->[0];
+            push @results, [ $symbol, $direction, $data->[1] ];
+        }
+    };
+
+    &$calculateIndicator('long');
+    &$calculateIndicator('short');
+
+    my @sorted = sort { $b->[2] <=> $a->[2] } @results ;
+    splice @sorted, $number_to_keep;
+
+    my $rv = { long => [], short => [] };
+    foreach my $item (@sorted) {
+        push @{ $rv->{long} }, $item->[0] if ($item->[1] eq 'long');
+        push @{ $rv->{short} }, $item->[0] if ($item->[1] eq 'short');
+    }
+    return $rv;
+}
+
+sub getSymbolsCounterTrend {
+}
+
+#Return list of all available symbols
+sub getAllSymbols {
+my $cfg         = Finance::HostedTrader::Config->new();
+
+return $cfg->symbols->all;
+}
+
+
+
+
+####OLD - Simpler and faster but less generic
+sub _getSymbolsTrendFollow {
     my $symbols = getAllSymbols();
     my @results;
     my $processor   = Finance::HostedTrader::ExpressionParser->new();
@@ -50,14 +132,4 @@ sub getSymbolsTrendFollow {
         push @{ $rv->{short} }, $item->[0] if ($item->[1] eq 'short');
     }
     return $rv;
-}
-
-sub getSymbolsCounterTrend {
-}
-
-#Return list of all available symbols
-sub getAllSymbols {
-my $cfg         = Finance::HostedTrader::Config->new();
-
-return $cfg->symbols->all;
 }
