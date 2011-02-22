289180aab012b8cf50ad6b5ce4a92f88b20a7d7b
Joao Costa <joaocosta@zonalivre.org>
2011-02-22 01:16:08 +0000

    Refactor data test outside of process test

--- data/fxcm/servers/checkProcesses.sh
+++ data/fxcm/servers/checkProcesses.sh
@@ -14,10 +14,6 @@ for p in Trader.pl RatePrinter.exe Server.exe; do
 done
 
 
-#check if data is up to date
-LAST_TICK=`mysql -N -ufxcm -e "SELECT MAX(datetime) FROM AUDUSD_300" fxcm`
-perl -MDate::Manip -e 'my $timediff=Delta_Format(DateCalc($ARGV[0], "now"),0,"%sh"); print "Datafeed out of date\n" if ($timediff > 1200)' "$LAST_TICK" 
-
 #check if the FXCM API server is responding to requests
 RES=`echo ask XAUUSD | nc -w 10 localhost 1500`
 if [[ "$RES" = "" ]]; then
