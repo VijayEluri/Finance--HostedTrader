d428a24fe5750ecd2a45dbb5e5df3d856b739916
Joao Costa <joaocosta@zonalivre.org>
2011-03-11 17:14:32 +0000

    Add symbol map to FXCM to convert from hostedtrader symbols into fxcm symbols

--- perl_modules/FXCMServer.pm
+++ perl_modules/FXCMServer.pm
@@ -75,6 +75,82 @@ has port => (
     required=> 1,
 );
 
+my %symbolMap = (
+    AUDCAD => 'AUD/CAD',
+    AUDCHF => 'AUD/CHF',
+    AUDJPY => 'AUD/JPY',
+    AUDNZD => 'AUD/NZD',
+    AUDUSD => 'AUD/USD',
+    AUS200 => 'AUS200',
+    CADCHF => 'CAD/CHF',
+    CADJPY => 'CAD/JPY',
+    CHFJPY => 'CHF/JPY',
+    CHFNOK => 'CHF/NOK',
+    CHFSEK => 'CHF/SEK',
+    EURAUD => 'EUR/AUD',
+    EURCAD => 'EUR/CAD',
+    EURCHF => 'EUR/CHF',
+    EURDKK => 'EUR/DKK',
+    EURGBP => 'EUR/GBP',
+    EURJPY => 'EUR/JPY',
+    EURNOK => 'EUR/NOK',
+    EURNZD => 'EUR/NZD',
+    EURSEK => 'EUR/SEK',
+    EURTRY => 'EUR/TRY',
+    EURUSD => 'EUR/USD',
+    GBPAUD => 'GBP/AUD',
+    GBPCAD => 'GBP/CAD',
+    GBPCHF => 'GBP/CHF',
+    GBPJPY => 'GBP/JPY',
+    GBPNZD => 'GBP/NZD',
+    GBPSEK => 'GBP/SEK',
+    GBPUSD => 'GBP/USD',
+    HKDJPY => 'HKD/JPY',
+    NOKJPY => 'NOK/JPY',
+    NZDCAD => 'NZD/CAD',
+    NZDCHF => 'NZD/CHF',
+    NZDJPY => 'NZD/JPY',
+    NZDUSD => 'NZD/USD',
+    SEKJPY => 'SEK/JPY',
+    SGDJPY => 'SGD/JPY',
+    TRYJPY => 'TRY/JPY',
+    USDCAD => 'USD/CAD',
+    USDCHF => 'USD/CHF',
+    USDDKK => 'USD/DKK',
+    USDHKD => 'USD/HKD',
+    USDJPY => 'USD/JPY',
+    USDMXN => 'USD/MXN',
+    USDNOK => 'USD/NOK',
+    USDSEK => 'USD/SEK',
+    USDSGD => 'USD/SGD',
+    USDTRY => 'USD/TRY',
+    USDZAR => 'USD/ZAR',
+    XAGUSD => 'XAG/USD',
+    XAUUSD => 'XAU/USD',
+    ZARJPY => 'ZAR/JPY',
+    ESP35 => 'ESP35',
+    FRA40 => 'FRA40',
+    GER30 => 'GER30',
+    HKG33 => 'HKG33',
+    ITA40 => 'ITA40',
+    JPN225 => 'JPN225',
+    NAS100 => 'NAS100',
+    SPX500 => 'SPX500',
+    SUI30 => 'SUI30',
+    SWE30 => 'SWE30',
+    UK100 => 'UK100',
+    UKOil => 'UKOil',
+    US30 => 'US30',
+    USOil => 'USOil',
+);
+
+sub _convertSymbolToFXCM {
+    my ($self, $symbol) = @_;
+
+    die("Unsupported symbol '$symbol'") if (!exists($symbolMap{$symbol}));
+    return $symbolMap{$symbol};
+}
+
 sub BUILD {
     my $self = shift;
     my $sock = IO::Socket::INET->new(
@@ -110,6 +186,7 @@ Returns the current ask(long) price for $symbol
 sub getAsk {
     my ($self, $symbol) = @_;
 
+    $symbol = $self->_convertSymbolToFXCM($symbol);
     return $self->_sendCmd("ask $symbol");
 }
 
@@ -122,6 +199,7 @@ Returns the current bid(short) price for $symbol
 sub getBid {
     my ($self, $symbol) = @_;
 
+    $symbol = _$self->convertSymbolToFXCM($symbol);
     return $self->_sendCmd("bid $symbol");
 }
 
@@ -143,6 +221,7 @@ $price   - The price at which the trade was executed.
 sub openMarket {
     my ($self, $symbol, $direction, $amount) = @_;
 
+    $symbol = $self->_convertSymbolToFXCM($symbol);
     my $data = $self->_sendCmd("openmarket $symbol $direction $amount");
     return split(/ /, $data);
 }
@@ -173,6 +252,7 @@ Eg, if baseUnit=10000, the symbol can only trade in multiples of 10000 (eg, 1500
 sub baseUnit {
     my ($self, $symbol) = @_;
 
+    $symbol = $self->_convertSymbolToFXCM($symbol);
     return $self->_sendCmd("baseunit $symbol");
 }
 
