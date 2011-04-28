26c90d8b0d2411831adb04c6668b23e1c4f353e0
Joao Costa <joaocosta@zonalivre.org>
2011-04-28 21:45:57 +0000

    HTML reports

--- data/fxcm/servers/Trader/report.pl
+++ data/fxcm/servers/Trader/report.pl
@@ -35,6 +35,8 @@ sub table_factory {
         $t = Text::ASCIITable->new( { headingText => $args{headingText} } );
         $t->setCols(@{ $args{cols}} );
     } elsif ($args{format} eq 'html') {
+        require HTML::Table;
+        $t = HTML::Table->new(-head => $args{cols});
     } else {
         die("unknown format: $args{format}");
     }
@@ -44,12 +46,6 @@ sub table_factory {
 
 my $account = Finance::HostedTrader::Factory::Account->new( SUBCLASS => $class, address => $address, port => $port)->create_instance();
 
-#my $account = Finance::HostedTrader::Account::FXCM->new(
-#                address => $address,
-#                port => $port,
-#              );
-
-my $trades = [];#$account->getTrades();
 my $nav = $account->getNav();
 
 
@@ -59,11 +55,7 @@ my $system = Finance::HostedTrader::Systems->new( name => 'trendfollow', account
 
 my $positions = $account->getPositions();
 
-my $t = table_factory( format=> 'text', headingText => 'Open Positions', cols => ['Symbol', 'Open Date','Size','Entry','Current','PL','%'] );
-
-my $h = HTML::Table->new(
-        -head => ['Symbol', 'Open Date','Size','Entry','Current','PL','%'],
-        );
+my $t = table_factory( format=> $format, headingText => 'Open Positions', cols => ['Symbol', 'Open Date','Size','Entry','Current','PL','%'] );
 
 foreach my $symbol (keys %$positions) {
 my $position = $positions->{$symbol};
@@ -83,29 +75,19 @@ foreach my $trade (@{ $position->trades }) {
         sprintf('%.2f', $baseCurrencyPL),
         $percentPL
     );
-    $h->addRow(
-        $trade->symbol,
-        $trade->openDate,
-        $trade->size,
-        $trade->openPrice,
-        $marketPrice,
-        sprintf('%.2f', $baseCurrencyPL),
-        $percentPL
-    );
 }
 }
 
+print "<html><body><p>" if ($format eq 'html');
 print "ACCOUNT NAV: " . $nav . "\n\n";
+print "</p>" if ($format eq 'html');
 print $t;
 
 print "\n";
 
 
 foreach my $system_name ( qw/trendfollow/ ) {
-    my $t = table_factory( format => 'text', headingText => $system_name, cols => ['Symbol','Market','Entry','Exit','Direction', 'Worst Case', '%']);
-    my $h = HTML::Table->new(
-        -head => ['Symbol','Market','Entry','Exit','Direction', 'Worst Case', '%'],
-        );
+    my $t = table_factory( format => $format, headingText => $system_name, cols => ['Symbol','Market','Entry','Exit','Direction', 'Worst Case', '%']);
     my $system = Finance::HostedTrader::Systems->new( name => $system_name, account => $account );
     my $data = $system->data;
     my $symbols = $data->{symbols};
@@ -124,61 +106,8 @@ foreach my $system_name ( qw/trendfollow/ ) {
                         sprintf('%.2f',$positionRisk),
                         sprintf('%.2f',100 * $positionRisk / $nav)
             );
-
-            $h->addRow( $symbol, 
-                        ($direction eq 'long' ? $account->getAsk($symbol) : $account->getBid($symbol)),
-                        $currentEntry,
-                        $currentExit,
-                        $direction,
-                        sprintf('%.2f',$positionRisk),
-                        sprintf('%.2f',100 * $positionRisk / $nav)
-            );
-
         }
     }
     print $t;
 }
-
-
-
-
-
-
-
-
-
-
-####OLD - Simpler and faster but less generic
-=pod
-use Finance::HostedTrader::ExpressionParser;
-
-sub _getSymbolsTrendFollow {
-    my $symbols = getAllSymbols();
-    my @results;
-    my $processor   = Finance::HostedTrader::ExpressionParser->new();
-
-    my $rv = { long => [], short => [] };
-
-    foreach my $symbol (@$symbols) {
-        my $data = $processor->getIndicatorData( {
-            'fields'          => "datetime,abs(trend(close,21)),trend(close,21)",
-            'symbol'        => $symbol,
-            'tf'            => 'week',
-            'maxLoadedItems'=> 41,
-            'numItems'      => 1,
-            'debug'         => 0,
-        });
-        $data = $data->[0];
-        push @results, [ $symbol, ($data->[2] > 0 ? 'long' : 'short'), $data->[1] ] if ($data->[1] > 1);
-    }
-
-    my @sorted = sort { $b->[2] <=> $a->[2] } @results ;
-    splice @sorted, 5;
-
-    foreach my $item (@sorted) {
-        push @{ $rv->{long} }, $item->[0] if ($item->[1] eq 'long');
-        push @{ $rv->{short} }, $item->[0] if ($item->[1] eq 'short');
-    }
-    return $rv;
-}
-=cut
+print "</body></html>" if ($format eq 'html');
