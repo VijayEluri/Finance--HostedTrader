81d779afeeff8854203fbf03031e349ba007eb58
Joao Costa <joaocosta@zonalivre.org>
2011-05-02 00:35:18 +0000

    Check for data

--- data/fxcm/servers/checkProcesses.sh
+++ data/fxcm/servers/checkProcesses.sh
@@ -24,8 +24,20 @@ else
     fi
 fi
 
+#Check there is enough memory (current version of Server.exe has a memory leak and needs restarting every now and then)
 FREEMEM=`free -m | grep Mem | cut -b 36-42`
 if [ $FREEMEM -lt 350 ]; then
     echo Running low on memory
     free -m
+fi
+
+# check there are enough records in the databse to process systems properly
+# this is necessary because the mysql fxcm database is memory only.
+# mysql restarts will wipe it out.
+# (there is a different process which checks if data is up to date)
+#TODO instead of hard coding this to a defined number of records on a specific symbol
+# load a System, then check it's  symbols and maxLoadedItems in the various sections
+DATA_RECORDS=`mysql -N -ufxcm -e "select count(1) from EURUSD_900" fxcm`
+if [ $DATA_RECORDS -lt 1900 ]; then
+    echo There don\'t seem to be enough data records in the fxcm database
 fi
