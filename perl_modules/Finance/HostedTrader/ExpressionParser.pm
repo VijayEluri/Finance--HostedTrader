c0cbfdfe6096eb213dde4ba8068171c6108f9c0b
João Costa <joaocosta@zonalivre.org>
2011-06-08 22:13:10 +0100

    Consolidate indicator+signals grammar into single definition

--- perl_modules/Finance/HostedTrader/ExpressionParser.pm
+++ perl_modules/Finance/HostedTrader/ExpressionParser.pm
@@ -43,53 +43,22 @@ $::RD_ERRORS = 1;
 sub new {
 
     my ( $class, $ds ) = @_;
-#TODO: grammar_indicators is a subset of grammar_signals. Having these duplicated is error prone.
-    my $grammar_indicators = q {
-start:          statement /\Z/               {$item[1]}
 
-statement:		<leftop: expression exp_sep expression > {join(' ', @{$item[1]})} |
+    my $grammar = q {
+start_indicator:          statement_indicator /\Z/               {$item[1]}
+
+start_signal:          statement_signal /\Z/               {$item[1]}
+
+statement_indicator:		<leftop: expression exp_sep expression > {join(' ', @{$item[1]})} |
 				expression
 
 exp_sep:	','
 
-expression:     term math_op expression      {"$item[1] $item[2] $item[3]"}
-              | term
+statement_signal:		<leftop: signal boolop signal > {join(' ', @{$item[1]})}
 
-math_op:        '+' | '-' | '*' | '/'
-
-term:           number
-              | field
-              | function
-              | '(' expression ')'        {"($item[2])"}
-
-number:         /\d+/
-
-field:			"datetime" | "open" | "high" | "low" | "close"
-
-function:
-		'ema(' expression ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("round(ta_ema($vals[0],$item[4]), 4)") } |
-		'sma(' expression ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("round(ta_sma($vals[0],$item[4]), 4)") } |
-		'rsi(' expression ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("round(ta_rsi($vals[0],$item[4]), 2)") } |
-		'max(' expression ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("ta_max($vals[0],$item[4])") } |
-		'min(' expression ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("ta_min($vals[0],$item[4])") } |
-		'tr(' ')'  { "T".Finance::HostedTrader::ExpressionParser::getID("round(ta_tr(high,low,close), 4)") } |
-		'atr(' number ')'  { "T".Finance::HostedTrader::ExpressionParser::getID("round(ta_ema(ta_tr(high,low,close),$item[2]), 4)") } |
-		'previous(' expression ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("ta_previous($vals[0],$item[4])") } |
-		'bolhigh(' expression ',' number ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); my ($t1,$t2) = Finance::HostedTrader::ExpressionParser::getID("ta_sma($vals[0],$item[4])","ta_sum(POW(($vals[0]) - ta_sma($vals[0], $item[4]), 2), $item[4])"); "round(T$t1 + $item[6]*SQRT(T$t2/$item[4]), 4)" } |
-		'bollow(' expression ',' number ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); my ($t1,$t2) = Finance::HostedTrader::ExpressionParser::getID("ta_sma($vals[0],$item[4])","ta_sum(POW(($vals[0]) - ta_sma($vals[0], $item[4]), 2), $item[4])"); "round(T$t1 - $item[6]*SQRT(T$t2/$item[4]), 4)" } |
-		'trend(' expression ',' number ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); my ($t1,$t2) = Finance::HostedTrader::ExpressionParser::getID("ta_sma($vals[0],$item[4])","ta_sum(POW($vals[0] - ta_sma($vals[0], $item[4]), 2), $item[4])"); "round(($vals[0] - T$t1) / (SQRT(T$t2/$item[4])), 2)" } |
-		'macd(' expression ',' number ',' number ',' number ')' {my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); my ($t1,$t2) = Finance::HostedTrader::ExpressionParser::getID("ta_ema($vals[0],$item[4])", "ta_ema($vals[0],$item[6])");"round(T$t1 - T$t2, 4)" } |
-		'macdsig(' expression ',' number ',' number ',' number ')' {my @vals=Finance::HostedTrader::ExpressionParser::checkArgs($item[2]);"T".Finance::HostedTrader::ExpressionParser::getID("round(ta_ema(ta_ema($vals[0],$item[4]) - ta_ema($item[2],$item[6]),$item[8]),4)") } |
-		'abs(' expression ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("round(abs($vals[0]), 4)") }
-};
-
-    my $grammar_signals = q {
-start:          statement /\Z/               {$item[1]}
-
-statement:		<leftop: signal boolop signal > {join(' ', @{$item[1]})}
+statement: statement_indicator | statement_signal
 
 boolop:	'AND' | 'OR'
-
 
 signal:         
 			    'crossoverup' '(' expression ',' expression ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[3],$item[5]);my ($t1,$t2) = Finance::HostedTrader::ExpressionParser::getID("ta_previous($vals[0],1)","ta_previous($vals[1],1)");"($item[3] > $item[5] AND T$t1 <= T$t2)"}
@@ -130,12 +99,10 @@ function:
 		'abs(' expression ')' { my @vals = Finance::HostedTrader::ExpressionParser::checkArgs($item[2]); "T".Finance::HostedTrader::ExpressionParser::getID("round(abs($vals[0]), 4)") }
 };
 
-    my $parser_indicators = Parse::RecDescent->new($grammar_indicators);
-    my $parser_signals    = Parse::RecDescent->new($grammar_signals);
+    my $parser    = Parse::RecDescent->new($grammar);
 
     my $self = {
-        '_parser_i' => $parser_indicators,
-        '_parser_s' => $parser_signals,
+        '_parser' => $parser,
         '_ds'       => ( $ds ? $ds : Finance::HostedTrader::Datasource->new() ),
         '_cache'    => {}, # caches parsing of expressions
         '_result_cache' => {}, # caches actual indicator values
@@ -186,7 +153,7 @@ sub getIndicatorData {
 #TODO: This shouldn't be global, I ought to have one of these per call
 #TODO: Refactor the parser bit so that it can be called independently. This will be usefull to validate expressions before running them.
         %INDICATORS = ();
-        $result     = $self->{_parser_i}->start($expr);
+        $result     = $self->{_parser}->start_indicator($expr);
 
 #TODO: Need a more meaningfull error message describing what's wrong with the given expression
         die("Syntax error in indicator \n\n$expr\n")
@@ -296,7 +263,7 @@ my ($self, $args) = @_;
       if ( !defined( $args->{maxLoadedItems} ));
 
     %INDICATORS = ();
-    my $result = $self->{_parser_s}->start( $args->{expr} );
+    my $result = $self->{_parser}->start_signal( $args->{expr} );
     die("Syntax error in signal \n\n$expr\n") unless ( defined($result) );
 
     my @fields = map { "$_ AS T$INDICATORS{$_}" } keys %INDICATORS;
