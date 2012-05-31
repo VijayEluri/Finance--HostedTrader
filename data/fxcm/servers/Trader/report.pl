8200b14434cb352613dab9673589cd915a1604fb
Joao Costa <joaocosta@zonalivre.org>
2012-05-31 17:03:33 +0000

    Generalise script arguments to match any account class

--- data/fxcm/servers/Trader/report.pl
+++ data/fxcm/servers/Trader/report.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use Data::Dumper;
-use Getopt::Long;
+use Getopt::Long qw(:config pass_through);
 
 use Finance::HostedTrader::Factory::Account;
 use Finance::HostedTrader::Trader;
@@ -12,18 +12,18 @@ use Finance::HostedTrader::System;
 use Finance::HostedTrader::Report;
 
 
-my ($address, $port, $class, $format) = ('127.0.0.1', 1500, 'FXCM', 'text');
+my ($class, $format, $pathToSystems) = ('ForexConnect', 'text', 'systems');
 
 GetOptions(
     "class=s"   => \$class,
-    "address=s" => \$address,
-    "port=i"    => \$port,
     "format=s"  => \$format,
+    "pathToSystems=s",  \$pathToSystems,
 );
 
 
-my $trendfollow = Finance::HostedTrader::System->new( name => 'trendfollow' );
-my $account = Finance::HostedTrader::Factory::Account->new( SUBCLASS => $class, address => $address, port => $port)->create_instance();
+my $trendfollow = Finance::HostedTrader::System->new( name => 'trendfollow', pathToSystems => $pathToSystems );
+my %classArgs = map { s/^--//; split(/=/) } @ARGV;
+my $account = Finance::HostedTrader::Factory::Account->new( SUBCLASS => $class, %classArgs)->create_instance();
 my $systemTrader = Finance::HostedTrader::Trader->new( system => $trendfollow, account => $account );
 my $report = Finance::HostedTrader::Report->new( account => $account, systemTrader => $systemTrader, format => $format );
 my $nav = $account->getNav();
