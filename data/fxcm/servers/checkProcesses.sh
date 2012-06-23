763f6a45128b0c949cff996608c554ccd400462b
Joao Costa <joaocosta@zonalivre.org>
2012-06-23 00:47:26 +0000

    Check Trader.pl is running

--- data/fxcm/servers/checkProcesses.sh
+++ data/fxcm/servers/checkProcesses.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+function checkProcess {
+   echo `ps aux | egrep "$1" | grep -vi defunct | grep -v egrep | wc -l`
+}
+
+for p in Trader.pl; do
+    VAR=$(checkProcess $p)
+    if [[ "$VAR" == "0"  ]]; then
+        echo Not running $p
+    fi
+done
 
 
 #Check there is enough memory
