856b7c9e7706aacb9b5c8055c0951533881b7c04
João Costa <joaocosta@zonalivre.org>
2012-12-16 14:12:37 +0000

    Updates to reflect renamed scripts

--- data/forexite/reboot.pl
+++ data/forexite/reboot.pl
@@ -24,8 +24,8 @@ foreach my $symbol ( @{$symbols} ) {
 }
 
 =pod
-updateTf.pl --verbose --available-timeframe=60 --timeframes=300
-updateTf.pl --verbose --available-timeframe=300 --timeframes=900,1800,3600
-updateTf.pl --verbose --available-timeframe=3600 --timeframes=7200,14400,86400
-updateTf.pl --verbose --available-timeframe=86400 --timeframes=604800
+fx-update-tf.pl --verbose --available-timeframe=60 --timeframes=300
+fx-update-tf.pl --verbose --available-timeframe=300 --timeframes=900,1800,3600
+fx-update-tf.pl --verbose --available-timeframe=3600 --timeframes=7200,14400,86400
+fx-update-tf.pl --verbose --available-timeframe=86400 --timeframes=604800
 =cut
